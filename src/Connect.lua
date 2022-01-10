c1c44fb0c2f9c852bd634d3bcf2664cb9839a315
lisachandra <lisachandra's email is private>
2022-01-10 15:37:28 +0700

    added connections metatable to try to fix `DisconnectAll` and `Destroy`

--- src/Connect.lua
+++ src/Connect.lua
@@ -1,9 +1,24 @@
 local strict = require(script.Parent:WaitForChild("strict"))
 local SignalService = require(script.Parent)
 
+local connectionsMt = {
+    __index = function(self, index)
+        if type(rawget(self, index)) == "function" then
+            return function(_self, ...)
+                return self[index](_self, ...)
+            end
+        end
+
+        return self[index]
+    end,
+
+    __newindex = function(self, index, value)
+        self[index] = value
+    end
+}
+
 local function createId(self)
 	math.randomseed(#self.__connections)
-    
 	return tostring(math.random(math.huge, 1))
 end
 
@@ -24,7 +39,8 @@ local function Connect(self, callbackFunction)
 	}
 
 	Connection = strict(Connection, "Connection")
-	self.__connections[id] = Connection
+	self.__connections[id] = setmetatable({}, connectionsMt)
+
 	return Connection
 end
 
