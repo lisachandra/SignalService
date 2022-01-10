c5c17549eccc0ff1e8b2b85daa43b2041714d6c8
lisachandra <lisachandra's email is private>
2022-01-10 14:11:48 +0700

    fixed indentation and fixed wait test

--- test/Tests/SignalService.spec.lua
+++ test/Tests/SignalService.spec.lua
@@ -1,169 +1,161 @@
 --# selene: allow(undefined_variable)
 return function()
-     local Packages = script.Parent.Parent:WaitForChild("Packages")
-     local SignalService = require(Packages:WaitForChild("SignalService"))
+    local Packages = script.Parent.Parent:WaitForChild("Packages")
+    local SignalService = require(Packages:WaitForChild("SignalService"))
 
-     describe("isSignal", function()
-          it("should be true", function()
-               local signalObject = setmetatable({}, {
-                    __tostring = function()
-                         return "Signal"
-                    end
-               })
+    describe("isSignal", function()
+        it("should be true", function()
+            local signalObject = setmetatable({}, {
+                __tostring = function()
+                        return "Signal"
+                end
+            })
 
-               expect(SignalService.isSignal(signalObject)).to.be.equal(true)
-          end)
+            expect(SignalService.isSignal(signalObject)).to.be.equal(true)
+        end)
 
-          it("should be false", function()
-               local fakeSignalObject = {}
-               expect(SignalService.isSignal(fakeSignalObject)).to.be.equal(false)
-          end)
-     end)
+        it("should be false", function()
+            local fakeSignalObject = {}
+            expect(SignalService.isSignal(fakeSignalObject)).to.be.equal(false)
+        end)
+    end)
 
-     describe("new", function()
-          it("should create a new signal", function(context)
-               local signalObject = SignalService.new()
-               context.addSignal(signalObject)
+    describe("new", function()
+        it("should create a new signal", function(context)
+            local signalObject = SignalService.new()
+            context.addSignal(signalObject)
 
-               expect(SignalService.isSignal(signalObject)).to.be.equal(true)
-          end)
-     end)
+            expect(SignalService.isSignal(signalObject)).to.be.equal(true)
+        end)
+    end)
 
-     describe("destroy", function()
-          it("should destroy the signal and disconnect all connections", function(context)
-               local signalObject = SignalService.new()
-               context.addSignal(signalObject)
+    describe("destroy", function()
+        it("should destroy the signal and disconnect all connections", function(context)
+            local signalObject = SignalService.new()
+            context.addSignal(signalObject)
 
-               local connection = signalObject:Connect(function()
-                    
-               end)
+            local connection = signalObject:Connect(function()
+                
+            end)
 
-               local connection2 = signalObject:Connect(function()
-                    
-               end)
+            local connection2 = signalObject:Connect(function()
+                
+            end)
 
-               signalObject:Destroy()
-               expect(connection.Connected).to.be.equal(false)
-               expect(connection2.Connected).to.be.equal(false)
-               expect(SignalService.isSignal(signalObject)).to.be.equal(false)
-          end)
-     end)
+            signalObject:Destroy()
+            expect(connection.Connected).to.be.equal(false)
+            expect(connection2.Connected).to.be.equal(false)
+            expect(SignalService.isSignal(signalObject)).to.be.equal(false)
+        end)
+    end)
 
-     describe("connect", function()
-          it("should return a connection", function(context)
-               local signalObject = SignalService.new()
-               context.addSignal(signalObject)
+    describe("connect", function()
+        it("should return a connection", function(context)
+            local signalObject = SignalService.new()
+            context.addSignal(signalObject)
 
-               local connection = signalObject:Connect(function()
-                    
-               end)
+            local connection = signalObject:Connect(function()
+                
+            end)
 
-               expect(SignalService.isSignal(connection)).to.be.equal(true)
-               expect(connection.Connected).to.be.equal(true)
-          end)
-     end)
+            expect(SignalService.isSignal(connection)).to.be.equal(true)
+            expect(connection.Connected).to.be.equal(true)
+        end)
+    end)
 
-     describe("fire", function()
-          it("should fire the signal with the correct arguments", function(context)
-               local signalObject = SignalService.new()
-               local stringArg
-               context.addSignal(signalObject)
+    describe("fire", function()
+        it("should fire the signal with the correct arguments", function(context)
+            local signalObject = SignalService.new()
+            local stringArg
+            context.addSignal(signalObject)
 
-               signalObject:Connect(function(arg1)
-                    stringArg = arg1
-               end)
+            signalObject:Connect(function(arg1)
+                stringArg = arg1
+            end)
 
-               signalObject:Fire("correct!")
-               expect(stringArg).to.be.equal("correct!")
-          end)
-     end)
+            signalObject:Fire("correct!")
+            expect(stringArg).to.be.equal("correct!")
+        end)
+    end)
 
-     describe("disconnect", function()
-          it("should disconnect the connection", function(context)
-               local signalObject = SignalService.new()
-               context.addSignal(signalObject)
+    describe("disconnect", function()
+        it("should disconnect the connection", function(context)
+            local signalObject = SignalService.new()
+            context.addSignal(signalObject)
 
-               local connection = signalObject:Connect(function() end)
+            local connection = signalObject:Connect(function() end)
 
-               connection:Disconnect()
-               expect(connection.Connected).to.be.equal(false)
-          end)
-     end)
+            connection:Disconnect()
+            expect(connection.Connected).to.be.equal(false)
+        end)
+    end)
 
-     describe("disconnectAll", function()
-          it("should disconnect all connections", function(context)
-               local signalObject = SignalService.new()
-               context.addSignal(signalObject)
+    describe("disconnectAll", function()
+        it("should disconnect all connections", function(context)
+            local signalObject = SignalService.new()
+            context.addSignal(signalObject)
 
-               local connection = signalObject:Connect(function() end)
+            local connection = signalObject:Connect(function() end)
 
-               local connection2 = signalObject:Connect(function() end)
+            local connection2 = signalObject:Connect(function() end)
 
-               signalObject:DisconnectAll()
-               expect(connection.Connected).to.be.equal(false)
-               expect(connection2.Connected).to.be.equal(false)
-          end)
-     end)
+            signalObject:DisconnectAll()
+            expect(connection.Connected).to.be.equal(false)
+            expect(connection2.Connected).to.be.equal(false)
+        end)
+    end)
 
-     describe("wait", function()
-          it("should yield the thread then when fired, resume with the correct arguments", function(context)
-               local signalObject = SignalService.new()
-               local args
-               context.addSignal(signalObject)
-
-               delay(1, function()
-                   signalObject:Fire("this", "is correct")
-                   repeat
-                       wait()
-                   until type(args) == "table"
-
-                   local string, string2 = args.string, args.string2
-                    
-                   print("printing wait args, ", string, string2)
-                   expect(string).to.be.equal("this")
-                   expect(string2).to.be.equal("is correct")
-               end)
-
-               args = coroutine.create(function()
-                   local string, string2 = signalObject:Wait()
-               end)
+    describe("wait", function()
+        it("should yield the thread then when fired, resume with the correct arguments", function(context)
+            local signalObject = SignalService.new()
+            local stringArg, stringArg1
+            context.addSignal(signalObject)
             
-               coroutine.resume(args)
-               args = getfenv(args)
-          end)
-     end)
+            coroutine.wrap(function()
+                stringArg, stringArg1 = signalObject:Wait()
+            end)()
 
-     describe("onDispatch", function()
-          it("should add a new dispatch", function(context)
-               local signalObject = SignalService.new()
-               context.addSignal(signalObject)
+            signalObject:Fire("this", "is correct")
+        
+            expect(stringArg).to.be.equal("this")
+            expect(stringArg1).to.be.equal("is correct")
+        end)
+    end)
 
-               signalObject:onDispatch({
+    describe("onDispatch", function()
+        it("should add a new dispatch", function(context)
+            local signalObject = SignalService.new()
+            context.addSignal(signalObject)
+
+            signalObject:onDispatch({
                     NewDispatch = function()
-                         
+                        
                     end
-               })
+            })
 
-               expect(signalObject.__dispatchHandler.NewDispatch).to.be.a("function")
-          end)
-     end)
+            expect(signalObject.__dispatchHandler.NewDispatch).to.be.a("function")
+        end)
+    end)
 
-     describe("dispatch", function()
-          it("should dispatch with the correct arguments", function(context)
-               local signalObject = SignalService.new()
-               context.addSignal(signalObject)
+    describe("dispatch", function()
+        it("should dispatch with the correct arguments", function(context)
+            local signalObject = SignalService.new()
+            local stringArg
+            context.addSignal(signalObject)
 
-               signalObject:onDispatch({
-                    NewDispatch = function(action)
-                         print(action.string)
-                         expect(action.string).to.be.equal("This is a dispatch running")
+            signalObject:onDispatch({
+                NewDispatch = function(action)
+                        stringArg = action.stringArg
                     end
-               })
+                }
+            )
+                
+            signalObject:Dispatch({
+                type = "NewDispatch",
+                stringArg = "This is a dispatch running"
+            })
 
-               signalObject:Dispatch({
-                    type = "NewDispatch",
-                    string = "This is a dispatch running"
-               })
-          end)
-     end)
+            expect(stringArg).to.be.equal("This is a dispatch running")
+        end)
+    end)
 end
