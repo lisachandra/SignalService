a195bd7355ba2f799ed33a856fdfef798c64203a
lisachandra <lisachandra's email is private>
2022-01-28 17:27:52 +0700

    Remove results variable

--- test/runner.server.lua
+++ test/runner.server.lua
@@ -2,10 +2,6 @@ local ReplicatedStorage = game:GetService("ReplicatedStorage")
 
 local TestEZ = require(ReplicatedStorage.TestEZ)
 
-local results = TestEZ.TestBootstrap:run({ ReplicatedStorage.Tests })
-
-if results.failureCount > 0 then
-	print("❌ Test run failed")
-else
-	print("✔️ All tests passed")
-end
+TestEZ.TestBootstrap:run({
+    ReplicatedStorage.Tests,
+})
