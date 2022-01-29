c7f3b0c2064ede18f1f39e84f0f46d45b351f1fd
lisachandra <lisachandra's email is private>
2022-01-29 19:52:42 +0700

    fix syntax

--- test/run.bash
+++ test/run.bash
@@ -3,7 +3,7 @@
 while true; do
     run-in-roblox --place test/signalservice-test.rbxlx --script test/runner.server.lua
 
-    if [[ "$(echo $?)" == 0]]; then
+    if [[ "$(echo $?)" == "0" ]]; then
         break
     fi
 
